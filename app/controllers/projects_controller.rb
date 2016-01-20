e1d393dde0edd42db2cd0c3169d10576e1e89603
tomasibarrab <tomas.ibarrab@gmail.com>
2016-01-19 19:39:44 -0700

    Projects CRUD done, added links to projects.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,48 @@
+class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
+	def index
+		@projects = Project.all.order('created_at DESC').paginate(page: params[:page], per_page:5)
+	end
+
+	def show
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(project_params)
+		if @project.save
+			redirect_to @project, notice: 'Project created'
+		else
+			render 'new', notice: 'Project was not created'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @project.update(project_params)
+			redirect_to @project, notice: 'Your project was successfully saved.'
+		else
+			render 'edit'
+		end
+	end	
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end
+	
+	private
+
+	def project_params
+		params.require(:project).permit(:title, :description, :link, :slug)
+	end
+
+	def find_project
+		@project = Project.friendly.find(params[:id])
+	end
+end
